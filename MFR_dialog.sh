76777468f6260d495589067039fdd6d7dff7100c
root <root@demo-tiki-asset0257.(none)>
2017-10-27 11:56:24 -0700

    more changes

--- MFR_dialog.sh
+++ MFR_dialog.sh
@@ -1,30 +1,52 @@
 #!/bin/bash
-DISTROS=$(whiptail --title "TOUCH DISPLAY MANUFACTURER" --radiolist \
-"Who is the touch display manufacturer?" 15 60 8 \
-"3M" "3M Multi-Touch Displays" ON \
-"AVOCOR" "Ultra HD 4K Interactive Touch Display" OFF \
-"DELL" "Dell Conference Room Monitor" OFF \
-"MSH" "Microsoft Surafce Hub Touch Display" OFF \
-"MT" "Multitaction" OFF \
-"OPTIKA" "Optika UHD Monitors" OFF \
-"PLANAR" "Planar Ultrares Displays" OFF \
-"SHARP" "Sharp AQUOS Board " OFF  3>&1 1>&2 2>&3)
+MFR=$(whiptail --title "TOUCH DISPLAY MANUFACTURER" --radiolist \
+"Who is the touch display manufacturer?" 25 67 19 \
+"3M55" "3M Multi-Touch Displays" OFF \
+"3M65" "3M Multi-Touch Displays" OFF \
+"Avocor-VTF7515" "Ultra HD 4K Interactive Touch Display" OFF \
+"Avocor-VTF8415" "Ultra HD 4K Interactive Touch Display" OFF \
+"D10-UE84" "Dell Conference Room Monitor" OFF \
+"DELL-C5518QT" "Dell Conference Room Monitor" OFF \
+"DELL-C8618QT" "Dell Conference Room Monitor" OFF \
+"SHUB55" "Microsoft Surafce Hub Touch Display" OFF \
+"SHUB84" "Microsoft Surafce Hub Touch Display" OFF \
+"MT553" "Multitaction" OFF \
+"Optika110" "Optika UHD Monitors" OFF \
+"Optika55UHD" "Optika UHD Monitors" OFF \
+"Optika65UHD" "Optika UHD Monitors" OFF \
+"Optika85UHD" "Optika UHD Monitors" OFF \
+"UR8450" "Planar Ultrares Displays" OFF \
+"UR8451" "Planar Ultrares Displays" OFF \
+"UR8651" "Planar Ultrares Displays" OFF \
+"UR9851" "Planar Ultrares Displays" OFF \
+"SHARPPNL803C" "Sharp AQUOS Board " OFF  3>&1 1>&2 2>&3)
 
 exitstatus=$?
 if [ $exitstatus = 0 ]; then
-    echo "$DISTROS"
+    echo "$MFR"
 else
     echo "You chose Cancel."
 fi
 
-case "$DISTROS" in
+case "$MFR" in
 
-3M) ;;
-AVOCOR) ;;
-DELL) ;;
-MSH) ;;
-MT) ;;
-OPTIKA) ;;
-PLANAR) ;;
-SHARP) ;;
+3M55) ;;
+3M65) ;;
+Avocor-VTF7515) ;;
+Avocor-VTF8415) ;;
+D10-UE84) ;;
+Dell-C5518QT) ;;
+Dell-C8618QT) ;;
+MT553) ;;
+Optika110) ;;
+Optika55UHD) ;;
+Optika65UHD) ;;
+Optika85UHD) ;;
+SharpPNL803C) ;;
+SHUB55) ;;
+SHUB84) ;;
+UR8450) ;;
+UR8451) ;;
+UR8651) ;;
+UR9851) ;;
 esac
--- display_cfg.sh
+++ display_cfg.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+if [ "$(whoami)" != "root" ]
+then
+    sudo su -s "$0"
+    exit
+fi
+
 GPU=$(whiptail --title "NVIDIA GRAPHICS CARD" --radiolist \
 "What is the Nvidia GPU Model for the controller?" 10 55 3 \
 "K5000" "NVIDIA K5000/K5200 GPU" OFF \
@@ -7,7 +14,7 @@ GPU=$(whiptail --title "NVIDIA GRAPHICS CARD" --radiolist \
 
 exitstatus=$?
 if [ $exitstatus = 0 ]; then
-    echo "$GPU"
+echo "$GPU"
 else
     echo "You chose Cancel."
 fi
@@ -17,6 +24,58 @@ case "$GPU" in
 K5000) ;;
 M4000) ;;
 P4000) ;;
+esac
+
+MFR=$(whiptail --title "TOUCH DISPLAY MANUFACTURER" --radiolist \
+"Who is the touch display manufacturer?" 25 67 19 \
+"3M55" "3M Multi-Touch Displays" ON \
+"3M65" "3M Multi-Touch Displays" OFF \
+"Avocor-VTF7515" "Ultra HD 4K Interactive Touch Display" OFF \
+"Avocor-VTF8415" "Ultra HD 4K Interactive Touch Display" OFF \
+"D10-UE84" "Dell Conference Room Monitor" OFF \
+"DELL-C5518QT" "Dell Conference Room Monitor" OFF \
+"DELL-C8618QT" "Dell Conference Room Monitor" OFF \
+"SHUB55" "Microsoft Surafce Hub Touch Display" OFF \
+"SHUB84" "Microsoft Surafce Hub Touch Display" OFF \
+"MT553" "Multitaction" OFF \
+"Optika110" "Optika UHD Monitors" OFF \
+"Optika55UHD" "Optika UHD Monitors" OFF \
+"Optika65UHD" "Optika UHD Monitors" OFF \
+"Optika85UHD" "Optika UHD Monitors" OFF \
+"UR8450" "Planar Ultrares Displays" OFF \
+"UR8451" "Planar Ultrares Displays" OFF \
+"UR8651" "Planar Ultrares Displays" OFF \
+"UR9851" "Planar Ultrares Displays" OFF \
+"SHARPPNL803C" "Sharp AQUOS Board " OFF  3>&1 1>&2 2>&3)
+
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+echo "$MFR"
+else
+    echo "You chose Cancel."
+fi
+
+case "$MFR" in
+
+3M55) ;;
+3M65) ;;
+Avocor-VTF7515) ;;
+Avocor-VTF8415) ;;
+D10-UE84) ;;
+Dell-C5518QT) ;;
+Dell-C8618QT) ;;
+MT553) ;;
+Optika110) ;;
+Optika55UHD) ;;
+Optika65UHD) ;;
+Optika85UHD) ;;
+SharpPNL803C) ;;
+SHUB55) ;;
+SHUB84) ;;
+UR8450) ;;
+UR8451) ;;
+UR8651) ;;
+UR9851) ;;
 esac
 
 CFG=$(whiptail --title "TOUCH DISPLAY CONFIGURATION" --radiolist \
@@ -30,7 +89,7 @@ CFG=$(whiptail --title "TOUCH DISPLAY CONFIGURATION" --radiolist \
 
 exitstatus=$?
 if [ $exitstatus = 0 ]; then
-    echo "$CFG"
+echo "$CFG"
 else
     echo "You chose Cancel."
 fi
@@ -45,3 +104,23 @@ case "$CFG" in
 4x3) ;;
 
 esac
+
+XORG=xorg.conf.template.$GPU.$MFR.$CFG
+GFX=gfx.cfg.template.$MFR.$CFG
+TUIO=tuio.cfg.template.$MFR.$CFG
+
+#REPLACE XORG TEMPLATE FILE
+sudo rm /etc/X11/xorg.conf
+sudo cp /etc/X11/$XORG /etc/X11/xorg.conf
+
+#REPLACE GFX TEMPLATE FILE
+sudo rm /home/runtime/.tsx/gfx.cfg
+sudo cp /home/runtime/.tsx/$GFX /home/runtime/.tsx/gfx.cfg
+
+#REPLACE TUIO TEMPLATE FILE
+sudo rm /home/runtime/.tsx/tuio.cfg
+sudo cp /home/runtime/.tsx/$TUIO /home/runtime/.tsx/tuio.cfg
+
+#echo "$XORG"
+#echo "$GFX"
+#echo "$TUIO"
