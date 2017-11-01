ef3fb19cbfe65f781438028df7deacfe88b47973
BS <bs@bsos>
2017-11-01 09:35:31 -0700

    update inogeni script

--- inogeni.sh
+++ inogeni.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-if [ "$(whoami)" != "root" ]; then sudo su -s "$0" fi
+if [ "$(whoami)" != "root" ]; then 
+	sudo su -s "$0" 
+	exit 
+fi
 
 #GLOBAL VARIABLES
 VID_DIR=/etc/udev/rules.d/tsx-video-devices.rules
@@ -8,32 +11,64 @@ DIR1=/sys/class/video4linux/video1/
 DIR2=/sys/class/video4linux/video2/
 
 inogeni_type () {
+
 DEVICE=$(whiptail --title "INOGENI CONFIGURATION" --radiolist \
-"What do you need to configure on the controller?" 10 50 2 \
+"Please select device for Inogeni ID $DEV." 10 50 2 \
 "plug_n_play" "" ON \
 "airplay" "" OFF 3>&1 1>&2 2>&3)
 
+#exitstatus=$?
+#if [ $exitstatus = 0 ]; then
+#echo "$CFG"
+#else
+#    echo "You chose Cancel."
+#fi
+
 case "$DEVICE" in
+
 plug_n_play) ;;
 airplay) ;;
-esac }
 
-if [ -d "$DIR0" ]; then DEV=$(cat /sys/class/video4linux/video0/name | cut -c 1-4)
-inogeni_type
-if [ $DEVICE = 'plug_n_play' ]; then VID_DEV=video_pnp0 else VID_DEV=video_airplay fi
-#sed -i 's/.*'$VID_DEV'.*/SUBSYSTEM=="video4linux", ATTR{name}=="'$DEV'-INOGENI DVIUSB", SYMLINK += "'$VID_DEV'"/' '$VID_DIR'
+esac 
+}
+
+if [ -d "$DIR0" ]; then 
+   DEV=$(cat /sys/class/video4linux/video0/name | cut -c 1-4)
+   inogeni_type
+	if [ $DEVICE = 'plug_n_play' ]; then 
+		VID_DEV=video_pnp0 
+	else 
+		VID_DEV=video_airplay 
+	fi
+echo dev0 is $VID_DEV
+sed -i 's/.*'$VID_DEV'.*/SUBSYSTEM=="video4linux", ATTR{name}=="'$DEV'-INOGENI DVIUSB", SYMLINK += "'$VID_DEV'"/' $VID_DIR
 fi
 
-if [ -d "$DIR1" ]; then DEV=$(cat /sys/class/video4linux/video1/name | cut -c 1-4)
-inogeni_type
-if [ $DEVICE = 'plug-n-play' ] && [ $VID_DEV = 'video_pnp0' ]; then VID_DEV=video_pnp1
-elif [ $DEVICE = 'plug-n-play' ]; then VID_DEV=video_pnp0
-else VID_DEV=video_airplay
-#sed -i 's/.*'$VID_DEV'.*/SUBSYSTEM=="video4linux", ATTR{name}=="'$DEV'-INOGENI DVIUSB", SYMLINK += "'$VID_DEV'"/' '$VID_DIR'
+if [ -d "$DIR1" ]; then 
+   DEV=$(cat /sys/class/video4linux/video1/name | cut -c 1-4)
+   inogeni_type
+	if [ $DEVICE = 'plug_n_play' ] && [ $VID_DEV = 'video_pnp0' ]; then 
+	   VID_DEV=video_pnp1
+	elif [ $DEVICE = 'plug_n_play' ] && [ $VID_DEV = 'video_airplay' ]; then 
+	   VID_DEV=video_pnp0
+	elif [ $DEVICE = 'plug_n_play' ]; then
+	   VID_DEV=video_pnp0
+	else 
+	   VID_DEV=video_airplay 
+	fi
+echo dev1 is $VID_DEV
+sed -i 's/.*'$VID_DEV'.*/SUBSYSTEM=="video4linux", ATTR{name}=="'$DEV'-INOGENI DVIUSB", SYMLINK += "'$VID_DEV'"/' $VID_DIR
 fi
 
-if [ -d "$DIR2" ]; then DEV=$(cat /sys/class/video4linux/video2/name | cut -c 1-4)
-inogeni_type
-if [ $DEVICE = 'plug-n-play' ]; then VID_DEV=video_pnp1 else VID_DEV=video_airplay fi
-#sed -i 's/.*'$VID_DEV'.*/SUBSYSTEM=="video4linux", ATTR{name}=="'$DEV'-INOGENI DVIUSB", SYMLINK += "'$VID_DEV'"/' '$VID_DIR'
+if [ -d "$DIR2" ]; then 
+   DEV=$(cat /sys/class/video4linux/video2/name | cut -c 1-4)
+   inogeni_type
+	if [ $DEVICE = 'plug_n_play' ]; then 
+	   VID_DEV=video_pnp1 
+	else 
+	   VID_DEV=video_airplay 
+	fi
+echo dev2 is $VID_DEV
+sed -i 's/.*'$VID_DEV'.*/SUBSYSTEM=="video4linux", ATTR{name}=="'$DEV'-INOGENI DVIUSB", SYMLINK += "'$VID_DEV'"/' $VID_DIR
 fi
+
